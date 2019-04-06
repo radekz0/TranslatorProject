6088c968d296888afc1846729ba34935b77b1da1
radekz0 <r.zielinski1221@gmail.com>
2019-04-06 17:36:29 +0200

    polishToGerman translation implemented.

--- src/main/java/com/azimo/internship/test/Main.java
+++ src/main/java/com/azimo/internship/test/Main.java
@@ -1,9 +1,12 @@
 package com.azimo.internship.test;
 
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Translator translator = new Translator();
-        System.out.println(translator.polish2English("/Users/radek/Downloads/hellokoding-courses-master/springboot-examples/TranslatorProject/src/main/resources/POL2ENG.txt", "rower"));
+        //System.out.println(translator.polish2English("/Users/radek/Downloads/hellokoding-courses-master/springboot-examples/TranslatorProject/src/main/resources/POL2ENG.txt", "rower"));
+        System.out.println(translator.polishToGerman("/Users/radek/Downloads/hellokoding-courses-master/springboot-examples/TranslatorProject/src/main/resources/POL2ENG.txt", "/Users/radek/Downloads/hellokoding-courses-master/springboot-examples/TranslatorProject/src/main/resources/ENG2GER.txt", "hjg"));
     }
 }
--- src/main/java/com/azimo/internship/test/Translator.java
+++ src/main/java/com/azimo/internship/test/Translator.java
@@ -8,9 +8,10 @@ import java.util.HashMap;
 class Translator {
 
     private HashMap<String, String> polEngMap = new HashMap<String, String>();
+    private HashMap<String, String> engGerMap = new HashMap<String, String>();
 
-    private void fileToMap(String polish2EnglishDictionaryPath) throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader(polish2EnglishDictionaryPath));
+    private void fileToMap(String dictionaryPath, HashMap hashMap) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(dictionaryPath));
         String fileLine;
 
         while ((fileLine = reader.readLine()) != null) {
@@ -18,21 +19,21 @@ class Translator {
             if (splitLine.length == 2) {
                 String key = splitLine[0];
                 String value = splitLine[1];
-                polEngMap.put(key, value);
+                hashMap.put(key, value);
             }
         }
         reader.close();
     }
 
-
      String polish2English(String polish2EnglishDictionaryPath, String polishExpression) {
-        try {
-            fileToMap(polish2EnglishDictionaryPath);
-        } catch (IOException e) {
-            System.out.println("Wrong input " + e.getMessage());
-        }
 
         String translation = "";
+
+         try {
+             fileToMap(polish2EnglishDictionaryPath, polEngMap);
+         } catch (IOException e) {
+             System.out.println("Wrong input " + e.getMessage());
+         }
 
         for(String key : polEngMap.keySet()){
             if(key.equals(polishExpression)){
@@ -42,5 +43,34 @@ class Translator {
         if(translation.isEmpty()) System.out.println("No such word in resources file.");
 
         return translation;
+    }
+
+    String polishToGerman(String polish2EnglishDictionaryPath, String english2GermanDictionaryPath, String polishExpression){
+
+        String engTranslation = "";
+        String translation = "";
+
+        try {
+            fileToMap(polish2EnglishDictionaryPath, polEngMap);
+            fileToMap(english2GermanDictionaryPath, engGerMap);
+        } catch (IOException e) {
+            System.out.println("Wrong input " + e.getMessage());
+        }
+
+        for(String key : polEngMap.keySet()){
+            if(key.equals(polishExpression)){
+                engTranslation = polEngMap.get(key);
+            }
+        }
+
+        for(String key : engGerMap.keySet()){
+            if(key.equals(engTranslation)){
+                translation = engGerMap.get(key);
+            }
+        }
+        if(translation.isEmpty()) System.out.println("No such word in resources file.");
+
+        return translation;
+
     }
 }
